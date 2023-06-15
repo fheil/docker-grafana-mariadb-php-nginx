633c4049dea1c8bcf94de118aa4046d97756cb7a
Frank H. Heil <fhheil@gmail.com>
2023-06-15 18:35:48 +0200

    Update set.sh

--- settings/set.sh
+++ settings/set.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sed -i 's/memory_limit = 2M/memory_limit= 32M/g' /etc/php81/php.ini
 sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 32M/g' /etc/php81/php.ini
+sed -i 's/post_max_size = 8M/post_max_size = 48M/g' /etc/php81/php.ini
 
 tail -f /dev/null
