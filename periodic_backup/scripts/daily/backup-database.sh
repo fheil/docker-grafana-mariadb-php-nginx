114d6b388881d06efe833adaa0c926e81ae0fc3f
Frank H. Heil <fhheil@gmail.com>
2023-06-16 17:33:55 +0200

    Update backup-database.sh

--- periodic_backup/scripts/daily/backup-database.sh
+++ periodic_backup/scripts/daily/backup-database.sh
@@ -4,7 +4,8 @@ BACKUP_FOLDER=/backup
 NOW=$(date '+%Y%m%d')
 
 GZIP=$(which gzip)
-MYSQLDUMP=$(which mysqldump)
+#MYSQLDUMP=$(which mysqldump)
+MARIADB-DUMP=$(which mariadb-dump)
 
 ### MySQL Server Login info ###
 MDB=$MYSQL_DATABASE
@@ -16,6 +17,6 @@ MBACKUP_DAYS=$MYSQL_KEEP_BACKUP_DAYS
 [ ! -d "$BACKUP_FOLDER" ] && mkdir --parents $BACKUP_FOLDER
 
 FILE=${BACKUP_FOLDER}/${NOW}_backup.sql.gz
-$MYSQLDUMP -h $MHOST -u $MUSER -p${MPASS} --databases $MDB | $GZIP -9 > $FILE
+$MARIADB-DUMP -h $MHOST -u $MUSER -p${MPASS} --databases $MDB | $GZIP -9 > $FILE
 
 find /backup/* -type f -mtime $MBACKUP_DAYS -name '*.gz' -delete
